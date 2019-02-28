f9c2e858a6009e4a74ee15751f3b4c99c3e8a364
Rahul Singh <rahul@altrosmart.com>
2019-02-28 21:10:40 +0530

    working with MQTT client

--- MQTT/App/apps.py
+++ MQTT/App/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
 
 
+
 class AppConfig(AppConfig):
     name = 'App'
+
+    def ready(self):
+        from .mqtt import client
+        client.loop_start()
--- MQTT/App/views.py
+++ MQTT/App/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .serializer import *
 
-# Create your views here.
+def get_data(data):
+    emp= Emp.objects.get(id=data['id'])
+    serial = EmpSerializer(emp)
+    return serial.data
